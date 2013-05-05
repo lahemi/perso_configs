55f58b18bc04e48b916786fc64203ad0e5b3200c
lahemi <lahemi.maki@gmail.com>
2013-05-05 07:47:15 +0300

    New theme+minor tweaking.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -16,9 +16,8 @@ local rckeys = require("rckeys")
 local unix_t = require("teatime/tea_unixclock")
 local hexc   = require("teatime/tea_hexclock")
 local osser  = require("teatime/osser")
---local memmo  = require("teatime/memmo")
 
--- {{{ Error handling
+-- Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
 if awesome.startup_errors then
@@ -41,43 +40,34 @@ do
         in_error = false
     end)
 end
--- }}}
 
 -- Variable definitions
 terminal = "lilyterm"
-editor = os.getenv("EDITOR") or "vim"
 modkey = "Mod4"
 mod1 = "Mod1"
 
 base_dir = os.getenv("HOME") .. ("/.config/awesome")
 themes_dir = (base_dir .. "/themes")
-beautiful.init(themes_dir .. "/modid/theme.lua")
+--beautiful.init(themes_dir .. "/grayscaled/theme.lua")
+beautiful.init(themes_dir .. "/catbug/theme.lua")
 if beautiful.wallpaper then
-    for s = 1, screen.count() do
-        gears.wallpaper.maximized(beautiful.wallpaper, s, true)
-    end
+    gears.wallpaper.maximized(beautiful.wallpaper, 1, true)
 end
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 -- Necessar to declare global, otherwise rckeys won't be able to see it.
--- (Without somehow returning this and then require in rckeys?)
-layouts =
-{
-    awful.layout.suit.floating,
-    awful.layout.suit.max,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.fair
-}
+layouts = { awful.layout.suit.floating,
+            awful.layout.suit.max,
+            awful.layout.suit.tile,
+            awful.layout.suit.tile.left,
+            awful.layout.suit.fair }
 
 tags = { layout = { layouts[2], layouts[2], layouts[2], layouts[2], layouts[3],
                     layouts[2], layouts[2], layouts[1], layouts[1], layouts[1],
                     layouts[2], layouts[2], layouts[5], layouts[2], layouts[1],
                     layouts[1], layouts[3], layouts[3] } }
 
-for s = 1, screen.count() do
-    tags[s] = awful.tag({1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18}, 1, tags.layout)
-end
+tags[1] = awful.tag({1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18}, 1, tags.layout)
 
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
@@ -90,19 +80,14 @@ memwig = wibox.widget.textbox()
 vicious.register(memwig, vicious.widgets.mem, "| MEM: $2MB ", 5)
 
 netwig = wibox.widget.textbox()             -- ip a and change to correspond your system.
-vicious.register(netwig, vicious.widgets.net, "| ↑ ${enp2s0 up_kb} ↓ ${enp2s0 down_kb} ", 1)
+vicious.register(netwig, vicious.widgets.net, "↑ ${enp2s0 up_kb} ↓ ${enp2s0 down_kb} ", 1)
 
---osswig = wibox.widget.textbox()
---vicious.register(osswig, vicious.teatime.osses, "| ♫ $1 dB | ", 1)
---hexclock = wibox.widget.textbox()
---vicious.register(hexclock, vicious.teatime.hexclock, "$1 ", 1.318)
-
---memmo     = memmo.new()
+-- Teatime.
 osser     = osser.new()
 hexclock  = hexc.new()
 unixclock = unix_t.new()
 
--- Create a wibox for each screen and add it
+-- Create a wibox and add it
 mywibox = {}
 mypromptbox = {}
 mylayoutbox = {}
@@ -130,60 +115,46 @@ mytasklist.buttons = awful.util.table.join(
                                                   client.focus = c
                                                   c:raise()
                                               end
-                                          end),
-                     awful.button({ }, 3, function ()
-                                              if instance then
-                                                  instance:hide()
-                                                  instance = nil
-                                              else
-                                                  instance = awful.menu.clients({ width=250 })
-                                              end
                                           end))
 
-for s = 1, screen.count() do
-    mypromptbox[1] = awful.widget.prompt()
-    mylayoutbox[1] = awful.widget.layoutbox(1)
-    mytaglist[1] = awful.widget.taglist(s, awful.widget.taglist.filter.all, mytaglist.buttons)
-    mytasklist[1] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
-    mywibox[1] = awful.wibox({ position = "top", screen = s, height = 18 })
+mypromptbox[1] = awful.widget.prompt()
+mylayoutbox[1] = awful.widget.layoutbox(1)
+mytaglist[1] = awful.widget.taglist(1, awful.widget.taglist.filter.all, mytaglist.buttons)
+mytasklist[1] = awful.widget.tasklist(1, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
+mywibox[1] = awful.wibox({ position = "top", screen = 1, height = 18 })
 
-    -- Lefties
-    local left_layout = wibox.layout.fixed.horizontal()
-    left_layout:add(mytaglist[s])
-    left_layout:add(mypromptbox[s])
+-- Lefties
+local left_layout = wibox.layout.fixed.horizontal()
+left_layout:add(mytaglist[1])
+left_layout:add(mypromptbox[1])
 
-    -- Righties
-    local right_layout = wibox.layout.fixed.horizontal()
-    right_layout:add(hexclock)
-    right_layout:add(netwig)
-    right_layout:add(cpuwig)
-    -- if s == 1 then right_layout:add(wibox.widget.systray()) end
---    right_layout:add(memmo)
-    right_layout:add(memwig)
-    right_layout:add(osser)
-    right_layout:add(unixclock)
-    right_layout:add(mylayoutbox[s])
+-- Righties
+local right_layout = wibox.layout.fixed.horizontal()
+right_layout:add(hexclock)
+right_layout:add(netwig)
+right_layout:add(cpuwig)
+right_layout:add(memwig)
+right_layout:add(osser)
+right_layout:add(unixclock)
+right_layout:add(mylayoutbox[1])
 
-    -- Now bring it all together (with the tasklist in the middle)
-    local layout = wibox.layout.align.horizontal()
-    layout:set_left(left_layout)
-    layout:set_middle(mytasklist[s])
-    layout:set_right(right_layout)
+-- Now bring it all together (with the tasklist in the middle)
+local layout = wibox.layout.align.horizontal()
+layout:set_left(left_layout)
+layout:set_middle(mytasklist[1])
+layout:set_right(right_layout)
+mywibox[1]:set_widget(layout)
 
-    mywibox[s]:set_widget(layout)
-end
 
 clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
     awful.key({ modkey,           }, "c",      function (c) c:kill()                         end),
-    awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "m",
         function (c)
             c.maximized_horizontal = not c.maximized_horizontal
             c.maximized_vertical   = not c.maximized_vertical
-        end)
-)
+        end))
 
 clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
@@ -193,7 +164,7 @@ clientbuttons = awful.util.table.join(
 -- Key bindings. They can be found in rckeys, huzzah for modularity.
 root.keys(rckeys)
 
--- {{{ Rules
+-- Rules
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = { },
@@ -201,14 +172,11 @@ awful.rules.rules = {
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
                      keys = clientkeys,
-                     buttons = clientbuttons } },
-}
--- }}}
+                     buttons = clientbuttons }}}
 
--- {{{ Signals
+-- Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function (c, startup)
-
     if not startup then
         -- Set the windows at the slave,
         awful.client.setslave(c)
@@ -225,7 +193,6 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
--- }}}
 
 -- Don't mind these.
 awful.util.spawn_with_shell("env bash ~/.rscripts/xmods.sh")
