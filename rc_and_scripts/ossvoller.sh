e9b08645bb21c4aaf6176aa33d0a67933291a78a
lahemi <lahemi.maki@gmail.com>
2013-07-01 00:47:15 +0300

    Minor code cleanup.

--- rc_and_scripts/ossvoller.sh
+++ rc_and_scripts/ossvoller.sh
@@ -3,7 +3,7 @@
 # GPLv2, 2013, Lauri Peltomäki, <lahemi.maki"at"gmail.com>
 
 # Adjust these variables to correspond with your system.
-GREEN="jack.green.front"
+DEV="jack.green.front"
 MUTE="jack.green.mute"
 
 
@@ -15,18 +15,19 @@ Usage: $0 <option> [argument]\n\n\
 \t-t or --toggle to toggle mute on and off.\n\
 \t-s or --setvol <number> to set the volume to the given level.\n\
 \t-c or --current to view the current volume.\n\n\
-Anything else will view this help text.\n"
+Anything else will show this help text.\n"
 }
 
+# Get the current volume level, -d for decimal, -i for int.
 function awk_vol() {
     case "$1" in
         -d)
             ossmix -c|\
-                awk 'BEGIN{FS=":"} /'$GREEN' / {printf $2}'
+                awk 'BEGIN{FS=":"} /'$DEV' / {printf $2}'
             return 0;;
         -i)
             ossmix -c|\
-                awk 'BEGIN{FS="."} /'$GREEN' / {printf substr($4,3,4)}'
+                awk 'BEGIN{FS="."} /'$DEV' / {printf substr($4,3,4)}'
             return 0;;
     esac
 }
@@ -42,39 +43,39 @@ function toggle_mute() {
 }
 
 function set_vol() {
-    ossmix $GREEN -- "$1"
+    ossmix $DEV -- "$1"
 }
 
 function increase() {
     TMPVOL=$(awk_vol -i)
     let "SETVOL = $TMPVOL + $1 + 1"
 
-    ossmix $GREEN $SETVOL
+    ossmix $DEV $SETVOL
 }
 
 function decrease() {
     TMPVOL=$(awk_vol -i)
     let "SETVOL = $TMPVOL - $1 + 1"
 
-    ossmix $GREEN $SETVOL
+    ossmix $DEV $SETVOL
 }
 
 function main() {
     case "$1" in
         '-i'|'--increase')
-            increase $2
+            increase "$2"
             return 0;;
         '-d'|'--decrease')
-            decrease $2
+            decrease "$2"
             return 0;;
         '-s'|'--setvol')
-            set_vol $2
+            set_vol "$2"
             return 0;;
         '-t'|'--toggle')
             toggle_mute
             return 0;;
         '-c'|'--current')
-            echo $(awk_vol -d)"dB"
+            printf "%sdB\n" $(awk_vol -d)
             return 0;;
         *)
             usage
