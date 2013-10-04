3e2fe2ad92c9bcf40550e64cd7a4effda3747703
lahemi <lahemi.maki@gmail.com>
2013-10-05 02:21:30 +0300

    Minor routine tweaking.

--- scripts/perso_utils.lua
+++ scripts/perso_utils.lua
@@ -1,4 +1,4 @@
-#!/usr/bin/env lua
+#!/usr/bin/env lua5.1
 -- Misc utility "library" for some tasks of my life.
 -- GPLv3, 2013, Lauri Peltomäki
 
@@ -7,7 +7,7 @@ perso_utils = {}
 -- Produce letter patterns.
 -- eg. lettermatch("title") => [tT][iI][tT][lL][eE]
 perso_utils.lettermatch = function(str, flag)
-    if str:find("[^%a]") then return end
+    if str:find("[^%a ]") then return end
 
     local flag = flag or "s" -- Return string or table.
 
@@ -18,24 +18,28 @@ perso_utils.lettermatch = function(str, flag)
         m = "[mM]", n = "[nN]", o = "[oO]", p = "[pP]",
         q = "[qQ]", r = "[rR]", s = "[sS]", t = "[tT]",
         u = "[uU]", v = "[vV]", w = "[wW]", x = "[xX]",
-        y = "[yY]", z = "[zZ]" }
+        y = "[yY]", z = "[zZ]", }
     local a = { "a","b","c","d","e","f","g","h","i",
                 "j","k","l","m","n","o","p","q","r",
-                "s","t","u","v","w","x","y","z" }
+                "s","t","u","v","w","x","y","z", }
 
     local pat = {}
 
     for i=1,#str do
         for j=1,#a do
             local ma = str:sub(i,i)
-            if ma:match(a[j]) then
-                pat[#pat+1] = m[a[j]]
+            if ma:match(" ") then
+                pat[#pat+1] = " "
+            else
+                if ma:match(a[j]) then
+                    pat[#pat+1] = m[a[j]]
+                end
             end
         end
     end
 
-    if flag == "s" then
-        return table.concat(pat)
+    if flag == "s" then  -- Workaround, to force a single whitespace
+        return table.concat(pat):gsub("%s+"," ")
     elseif flag == "t" then
         return pat
     else
@@ -47,8 +51,7 @@ end
 -- function, with some "error handling".
 perso_utils.filereadall = function(file)
     local ret = ""
-    if not file then -- Magicks of empty if body.
-    else
+    if file then
         local fh = io.open(file)
         if not fh then 
             ret = "File handle not handled!"
@@ -56,14 +59,14 @@ perso_utils.filereadall = function(file)
             ret = fh:read('*a')
             fh:close()
         end
-        return ret
-    end
+    else ret = nil end
+
+    return ret
 end
 
 -- Primitive <html> stripping, not pretty at all, but works.
 perso_utils.brutalhtmlstrip = function(file)
-    if not file then
-    else
+    if file then
         local cont = {}
         for line in io.lines(file) do
             cont[#cont+1] = string.gsub(line,"<(.-)>","")
@@ -101,6 +104,17 @@ perso_utils.wraps = function(str, limit, sep, flag)
     else
         return nil
     end
+end
+
+perso_utils.getpage = function(url)
+    local http = require("socket.http")
+    local page,err = ""
+    local page,err = http.request(url)
+    if not page then
+        print("Error fetching the page: ", err)
+    else
+        return page
+    end 
 end
 
 
