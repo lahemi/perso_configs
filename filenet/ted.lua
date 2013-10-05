a379471532ce9a6bd2407b31826f9c929c384b0c
lahemi <lahemi.maki@gmail.com>
2013-10-05 14:49:13 +0300

    Init TED.

---
+++ filenet/ted.lua
@@ -0,0 +1,27 @@
+#!/usr/bin/env lua5.1
+-- Part of the filenet project.
+-- Small helper for TED talks, picks the video url for you.
+-- GPLv3, 2013, Lauri Peltomäki
+
+local http = require("socket.http")
+local print  = print
+local assert = assert
+local string = { find = string.find,
+                 gsub = string.gsub,
+                 match = string.match,
+                 gmatch = string.gmatch, }
+
+ted = function()
+    local page = assert(http.request(arg[1]))
+
+    for line in page:gmatch("[^\n]+") do
+        if line:find("no%-flash%-video%-download") then
+            local ret = line:gsub("^%s+","")
+                            :gsub("%s+$","")
+                            :match('(http.-)"')
+            return ret
+        end
+    end
+end
+
+print(ted())
