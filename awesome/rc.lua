87cb2790f2c8c86a2222189795908bb0ec58a5b7
lahemi <lahemi.maki@gmail.com>
2013-10-10 23:58:54 +0300

    Redundancy removal.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -44,14 +44,14 @@ end
 terminal = "sakura"
 mod4 = "Mod4"
 
-base_dir = os.getenv("HOME") .. ("/.config/awesome")
-themes_dir = (base_dir .. "/themes")
-beautiful.init(themes_dir .. "/grayscaled/theme.lua")
---beautiful.init(themes_dir .. "/A_rch/theme.lua")
---beautiful.init(themes_dir .. "/catbug/theme.lua")
+base_dir = os.getenv'HOME'..'/.config/awesome'
+themes_dir = base_dir..'/themes'
+beautiful.init(themes_dir..'/grayscaled/theme.lua')
+--beautiful.init(themes_dir..'/A_rch/theme.lua')
+--beautiful.init(themes_dir..'/catbug/theme.lua')
 if beautiful.wallpaper then
     for s=1,screen.count() do
-        gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+        gears.wallpaper.maximized(beautiful.wallpaper,s,true)
     end
 end
 
@@ -64,10 +64,10 @@ layouts = { awful.layout.suit.floating,
             awful.layout.suit.fair,
             awful.layout.suit.tile.bottom, }
 tags = {}
-thetags = { layout = { layouts[2], layouts[2], layouts[2], layouts[2], layouts[3],
-                       layouts[2], layouts[2], layouts[1], layouts[1], layouts[1],
-                       layouts[2], layouts[2], layouts[5], layouts[2], layouts[1],
-                       layouts[1], layouts[3], layouts[3] } }
+thetags = { layout = { layouts[2],layouts[2],layouts[2],layouts[2],layouts[3],
+                       layouts[2],layouts[2],layouts[1],layouts[1],layouts[1],
+                       layouts[2],layouts[2],layouts[5],layouts[2],layouts[1],
+                       layouts[1],layouts[3],layouts[3] }}
 for s=1,screen.count() do
     tags[s] = awful.tag({1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18}, s, thetags.layout)
 end
@@ -82,57 +82,51 @@ mypromptbox = {}
 mylayoutbox = {}
 mytaglist = {}
 mytaglist.buttons = awful.util.table.join(
-                    awful.button({ }, 1, awful.tag.viewonly),
-                    awful.button({ mod4 }, 1, awful.client.movetotag),
-                    awful.button({ }, 3, awful.tag.viewtoggle),
-                    awful.button({ mod4 }, 3, awful.client.toggletag)
-                    )
+                    awful.button({},1,awful.tag.viewonly),
+                    awful.button({mod4},1,awful.client.movetotag),
+                    awful.button({},3,awful.tag.viewtoggle),
+                    awful.button({mod4},3,awful.client.toggletag))
 mytasklist = {}
 mytasklist.buttons = awful.util.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if c == client.focus then
-                                                  c.minimized = true
-                                              else
-                                                  -- Without this, the following
-                                                  -- :isvisible() makes no sense
-                                                  c.minimized = false
-                                                  if not c:isvisible() then
-                                                      awful.tag.viewonly(c:tags()[1])
-                                                  end
-                                                  -- This will also un-minimize
-                                                  -- the client, if needed
-                                                  client.focus = c
-                                                  c:raise()
-                                              end
-                                          end))
+                     awful.button({},1,
+                        function(c)
+                            if c == client.focus then c.minimized = true
+                            else
+                                c.minimized = false
+                                if not c:isvisible() then
+                                    awful.tag.viewonly(c:tags()[1])
+                                end
+                                client.focus = c
+                                c:raise()
+                            end end))
 
 for s=1,screen.count() do
     mypromptbox[s] = awful.widget.prompt()
     mylayoutbox[s] = awful.widget.layoutbox(s)
-    mytaglist[s] = awful.widget.taglist(s, awful.widget.taglist.filter.all, mytaglist.buttons)
-    mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
-    mywibox[s] = awful.wibox({ position = "top", screen = s, height = 18 })
+    mytaglist[s]   = awful.widget.taglist(s,awful.widget.taglist.filter.all,mytaglist.buttons)
+    mytasklist[s]  = awful.widget.tasklist(s,awful.widget.tasklist.filter.currenttags,mytasklist.buttons)
+    mywibox[s]     = awful.wibox({position = "top",screen = s,height = 18})
 
     -- Lefties
-    local left_layout = wibox.layout.fixed.horizontal()
-    left_layout:add(mytaglist[s])
-    left_layout:add(mypromptbox[s])
+    local llayout = wibox.layout.fixed.horizontal()
+    llayout:add(mytaglist[s])
+    llayout:add(mypromptbox[s])
 
     -- Righties
-    local right_layout = wibox.layout.fixed.horizontal()
-    right_layout:add(hexclock)
-    right_layout:add(unixclock)
-    right_layout:add(boxen.cpubox)
-    right_layout:add(boxen.ossbox)
-    right_layout:add(boxen.netbox)
-    right_layout:add(boxen.membox)
-    right_layout:add(mylayoutbox[s])
+    local rlayout = wibox.layout.fixed.horizontal()
+    rlayout:add(hexclock)
+    rlayout:add(unixclock)
+    rlayout:add(boxen.cpubox)
+    rlayout:add(boxen.ossbox)
+    rlayout:add(boxen.netbox)
+    rlayout:add(boxen.membox)
+    rlayout:add(mylayoutbox[s])
 
     -- Now bring it all together (with the tasklist in the middle)
     local layout = wibox.layout.align.horizontal()
-    layout:set_left(left_layout)
+    layout:set_left(llayout)
     layout:set_middle(mytasklist[s])
-    layout:set_right(right_layout)
+    layout:set_right(rlayout)
     mywibox[s]:set_widget(layout)
 end
 
@@ -142,7 +136,7 @@ root.keys(modekeys)
 -- Rules
 awful.rules.rules = {
     -- All clients will match this rule.
-    { rule = { },
+    { rule = {},
       properties = { border_width = beautiful.border_width,
                      border_color = beautiful.border_normal,
                      focus = awful.client.focus.filter,
@@ -166,9 +160,5 @@ client.connect_signal("manage", function (c, startup)
     local titlebars_enabled = false
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("focus",   function(c) c.border_color = beautiful.border_focus  end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
-
--- Don't mind these.
-awful.util.spawn_with_shell("env bash ~/.rscripts/xmods.sh")
-awful.util.spawn_with_shell("env bash ~/.rscripts/bamboo.sh")
