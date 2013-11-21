2aeade4e11c00f1d68211a072ce58cac50d4b7d6
lahemi <lahemi.maki@gmail.com>
2013-11-21 12:29:30 +0200

    Init cd shortcuts by name.Ã

---
+++ rc_conf/mksh_funcs/bb.sh
@@ -0,0 +1,16 @@
+# You can go up(towards /) by just typing bb dirname.
+# eg. [/home/user/dir1/subdir2]$ bb home -> [/home]$
+# Source or add this in your shell's rc.
+# This should work with Bash too.
+
+bb() {
+    # No args == cd
+    cd $(awk -v p=$(pwd) -v a="$1" '
+        BEGIN{
+            if(p ~ a) {
+                i=index(p,a);
+                e=length(a);
+                print(substr(p,1,(i+e-1)));
+            }
+        }')
+}
