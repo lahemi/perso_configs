63b273bca7ad5748b0ac1ad33886935cf56e7585
lahemi <lahemi.maki@gmail.com>
2014-10-01 18:41:16 -0400

    Modify cd to keep a list of visited directories, and add a func to show those.

---
+++ rc_conf/mksh_funcs/cd.sh
@@ -0,0 +1,19 @@
+# Keep a list of visited directories.
+PATHHIST=()
+cd() {
+    builtin cd "$@"
+
+    # Keeping the limit of how many to remember quite small.
+    [[ ${#PATHHIST[@]} -gt 20 ]] && PATHHIST=()
+    PATHHIST+=("$PWD")
+}
+
+# Given a num, show only that-th dirname.
+lsdirs() {
+    case "$1" in
+        +([0-9])) print "${PATHHIST[$1]}";;
+        *) for p in "${PATHHIST[@]}"; do
+               print "$p"
+           done;;
+    esac
+}
